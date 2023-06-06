d9d901a1f43deddb9117b33734e81b4a85d050c0
wulang <wulang@dongzouxizou.com>
2023-06-06 14:29:54 +0800

    提交

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#jsonvalidate").click(function(){
+        alert("jsn");
+      $(".content").html("/html/jsonvalidate.html");
+    });
+
+    $("#pictopdf").click(function(){
+        alert("pictopdf");
+      });
+
+      $("#wordtopdf").click(function(){
+        alert("wordtopdf");
+      });
+
+      $("#filedecompress").click(function(){
+        alert("filedecompress");
+      });
+  });
