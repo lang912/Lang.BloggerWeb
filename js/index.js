4a5072cf6dada68b1a6308374db66bc84449ec37
wulang <wulang@dongzouxizou.com>
2023-06-12 11:49:07 +0800

    菜单切换调整

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
+    $("#jsonvali").on("click", function () {
+        $(".content").load("../html/jsonvalidate.html");
+    });
 
+    $(".dropdown-content a").on("click", function () {
+        var name=$(this).attr("id");
+        alert(name);
+        $(".content").load("../html/"+name+".html");
+    });
 });
