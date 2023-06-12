5db672e4f6a69ffb392f7faac71fb394fa7ab5d1
wulang <wulang@dongzouxizou.com>
2023-06-12 14:02:19 +0800

    变更菜单栏切换方式

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
         $(".content").load("../html/jsonvalidate.html");
     });
 
+    // 绑定a标签切换内容
     $(".dropdown-content a").on("click", function () {
         var name=$(this).attr("id");
-        alert(name);
         $(".content").load("../html/"+name+".html");
     });
 });
