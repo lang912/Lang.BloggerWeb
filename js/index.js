60d7d9d5cd0195355495489bd4382fc615c519c7
wulang <wulang@dongzouxizou.com>
2023-06-12 17:56:23 +0800

    点击事件绑定

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 $(document).ready(function () {
-    $("#jsonvali").on("click", function () {
-        $(".content").load("../html/jsonvalidate.html");
-    });
-
     // 绑定a标签切换内容
-    $(".dropdown-content a").on("click", function () {
+    $(".main ul a[id]").on("click", function () {
         var name=$(this).attr("id");
         $(".content").load("../html/"+name+".html");
     });
